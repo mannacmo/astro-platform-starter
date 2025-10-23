c55ea394c0638a1b2839d0bf6c69475ae4fcd878
mannacmo <manna_2024@naver.com>
2025-10-23 13:52:43 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import netlify from '@astrojs/netlify/functions';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
   adapter: netlify(),
 });
