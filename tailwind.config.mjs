951d9f2ed5ec20d8660f864295fb5a496bcf3aa2
mannacmo <manna_2024@naver.com>
2025-10-23 14:01:31 +0900

    Delete tailwind.config.mjs

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tailwindcss'
-
-export default defineConfig({
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-})
